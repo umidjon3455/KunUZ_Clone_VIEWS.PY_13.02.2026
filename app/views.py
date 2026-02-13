c44069d0bdb435d7fd270c0f0ddfd52947cbcd28
umidjon3455 <umidjonabdullayev511@gmail.com>
2026-02-13 19:42:10 +0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import News
 
 def home(request):
-    news = News.objects.all()
+    news = News.objects.all().order_by('-id')
     return render(request, 'home.html', {'news': news})
 
 
 def news_detail(request, slug):
     news = get_object_or_404(News, slug=slug)
     return render(request, 'detail.html', {'news': news})
+
 
